f4c615c86d7206f8b0eed4bcb7ed03bfb96c4972
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-07-13 00:41:40 +0530

    Added solution - LeetHub

--- Find the element that appears once - GFG/find-the-element-that-appears-once.cpp
+++ Find the element that appears once - GFG/find-the-element-that-appears-once.cpp
@@ -7,12 +7,17 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-	int search(int A[], int N){
-	    int res=0;
-	    for(int i=0;i<N;i++)
-	    res^=A[i];
-	    
-	    return res;
+	int search(int arr[], int n){
+        int left = 0, right = n - 1;
+        while(left < right){
+            int mid = (left + right)/2;
+            if((mid % 2 == 0 && arr[mid] == arr[mid + 1]) || (mid % 2 == 1 && arr[mid] == arr[mid - 1]))
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        
+        return arr[left];
 	}
 };
 

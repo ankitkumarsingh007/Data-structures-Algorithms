f793bfecfacda6e0b5bc7c91ff9bb3ceb205ff5f
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-07-19 22:32:34 +0530

    Time: 2 ms (46.06%), Space: 6.5 MB (31.97%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> res;
+        
+        for(int i=0;i<n;i++){
+            vector<int> v(i+1,1);
+            
+            for(int j=1;j<i;j++)
+                v[j]=res[i-1][j-1]+res[i-1][j];
+            
+            res.push_back(v);
+        }
+        
+        return res;
+    }
+};
+
+

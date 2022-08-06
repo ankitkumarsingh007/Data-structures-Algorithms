a0f024793310cec7ffb73a574b86aa9cde53d5e7
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-08-06 22:58:18 +0530

    Time: 4 ms (25.82%), Space: 6.1 MB (22.55%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int pigs = 0;
-        while (pow(minutesToTest / minutesToDie + 1, pigs) < buckets) {
-            pigs += 1;
-        }
-        return pigs;
-    }
+int poorPigs(int buckets, int poisonTime, int totalTime) {
+    return ceil(log(buckets) / log(totalTime / poisonTime + 1));
+}
 };

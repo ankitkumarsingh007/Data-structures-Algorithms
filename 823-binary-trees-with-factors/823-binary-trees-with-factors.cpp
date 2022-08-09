24c7c084c4e1f9805d14f1f4407b36a615dd69cc
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-08-09 16:53:49 +0530

    Time: 827 ms (5.23%), Space: 9.3 MB (67.38%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -15,18 +15,20 @@ public:
         ll res=1;
         
         for(ll i=1;i<n;i++){
-            ll cnt=1,a=arr[i];
+            ll cnt=1,a=arr[i],j;
             
-            for(ll j=2;j*j<=a;j++){
+            for(j=2;j*j<a;j++){
                 if(a%j==0){
                     if(mp.find(j)!=mp.end() && mp.find(a/j)!=mp.end())
-                        if(j!=(a/j))
-                            cnt+=(mp[j]*mp[a/j]*2);
-                        else
-                            cnt+=(mp[j]*mp[j]);
-                        cnt%=m;
+                        // if(j!=(a/j))
+                            cnt=(cnt+mp[j]*mp[a/j]*2)%m;
+                        // else
+                        //     cnt+=(mp[j]*mp[j]);
+                        // cnt%=m;
                 }
             }
+            if(j*j==a)
+                cnt=(cnt+mp[j]*mp[a/j])%m;
             
             mp[a]=cnt;
             res=(res+cnt)%m;

7c8f14cb02b73229146ec9f0cccb92619f47865f
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-08-09 16:49:51 +0530

    Time: 789 ms (5.23%), Space: 9.3 MB (67.38%) - LeetHub

---
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    typedef long long ll;
+    int m=1e9+7;
+    
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        ll n=arr.size();
+        sort(arr.begin(),arr.end());
+        unordered_map<ll,ll> mp;
+        
+        if(n==1)
+            return 1;
+        
+        mp[arr[0]]=1;
+        ll res=1;
+        
+        for(ll i=1;i<n;i++){
+            ll cnt=1,a=arr[i];
+            
+            for(ll j=2;j*j<=a;j++){
+                if(a%j==0){
+                    if(mp.find(j)!=mp.end() && mp.find(a/j)!=mp.end())
+                        if(j!=(a/j))
+                            cnt+=(mp[j]*mp[a/j]*2);
+                        else
+                            cnt+=(mp[j]*mp[j]);
+                        cnt%=m;
+                }
+            }
+            
+            mp[a]=cnt;
+            res=(res+cnt)%m;
+        }
+        
+        return res;
+    }
+};

c6ee86b8cd2831161f76ed1cca9b1d84b83f7d97
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-08-15 18:46:54 +0530

    Time: 5 ms (33.70%), Space: 6.7 MB (65.95%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    typedef long long ll;
+    int combinationSum4(vector<int>& arr, int sum) {
+        sort(arr.begin(),arr.end());
+        ll mod=INT_MAX;
+        
+        vector<ll> dp(sum+1,0);
+        dp[0]=1;
+        
+        for(int i=1;i<=sum;i++){
+            for(int x:arr){
+                if(x<=i)
+                    dp[i]=(dp[i]%mod+dp[i-x]%mod)%mod;
+                else
+                    break;
+            }
+        }
+        
+        return dp[sum];
+    }
+};

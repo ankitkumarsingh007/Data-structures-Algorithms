adfd4b66aa1336ed9039e749687c71f49d6a4b69
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-08-15 18:54:07 +0530

    Time: 7 ms (19.94%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     typedef long long ll;
     int combinationSum4(vector<int>& arr, int sum) {
-        ll mod=INT_MAX;
-        
         vector<ll> dp(sum+1,0);
         dp[0]=1;
         
         for(int i=1;i<=sum;i++){
             for(int x:arr){
                 if(x<=i){
-                    dp[i]=(dp[i]+dp[i-x]);
+                    dp[i]+=dp[i-x];
+                    //bcz if it's out of range of 32 bit then it won't be part of answer
+                    //as it's mentioned specially that result will be in 32 bit range
                     if(dp[i]>=INT_MAX)
                         dp[i]=-1;
                 }    

7e18a636967c49aef037432aa5144ffa4f125fb3
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-08-23 19:22:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n<=0)
-            return false;
-        
-        if((n&(n-1))!=0)
-            return false;
-        
-        int cnt=0;
-        while((n&1)!=1){
-            n>>=1;
-            cnt++;
-        }
-        
-        return cnt%2==0;
+        return (n>0) && (n&(n-1))==0 && (n-1)%3==0;
     }
 };

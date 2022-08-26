97cf3ecd671ff887815d0a3cd49e455f5e01b9fa
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-08-26 15:56:20 +0530

    Time: 551 ms (6.21%), Space: 6.2 MB (45.48%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string s;
+    int x,size;
+    
+    bool helper(int l,int r){
+        if(l==r){
+            x=stoi(s);
+            
+            if(int(log10(x)+1)==size && (x&(x-1))==0)
+                return true;
+            else
+                return false;
+        }
+        
+        for(int i=l;i<=r;i++){
+            swap(s[l],s[i]);
+            if(helper(l+1,r)==true)
+                return true;
+            swap(s[l],s[i]);
+        }
+        
+        return false;
+    }
+    
+    bool reorderedPowerOf2(int n) {
+        s=to_string(n);
+        size=s.size();
+        return helper(0,size-1);
+    }
+};

f7623b049960a753c1f1d8f73fb6cf05fe0f9cba
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-09-12 16:50:46 +0530

    Time: 11 ms (52.87%), Space: 10.6 MB (27.39%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
+        //(1) First sort tokens array so that we buy higher powers with same score
+        //(2) & then sell cheaper ones to earn scores easily
         sort(tokens.begin(),tokens.end());
         
+        //Corner cases
         if(tokens.size()==0 || tokens[0]>power)
             return 0;
         
         power-=tokens[0];
         int res=1,curr=1,i=1,j=tokens.size()-1;
         
+        //simple 2 pointer approach is applied here
         while(i<=j){
             if(power>=tokens[i]){
                 power-=tokens[i++];

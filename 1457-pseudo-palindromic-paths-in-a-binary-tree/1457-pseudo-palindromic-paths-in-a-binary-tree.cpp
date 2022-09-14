d088aa0f98944206699733326af92160bcd4abf2
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-09-14 17:22:44 +0530

    Time: 539 ms (64.64%), Space: 179.3 MB (91.11%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -11,36 +11,31 @@
  */
 class Solution {
 public:
+    //main idea is at most 1 odd occurence will e allowed
+    
     int res=0;
-    vector<int> cnt;
+    int cnt=0;
     
     void preOrder(TreeNode* root){
         if(root==NULL)
             return;
         
-        cnt[root->val]++;
+        cnt^= (1<<root->val);
+        
         if(root->left==NULL && root->right==NULL){
-            int odd=0;
-            
-            for(int i=1;i<=9;i++){
-                if(cnt[i]%2==1)
-                    odd++;
-            }
-            
-            if(odd<=1)
+            if(cnt==0 || (cnt&(cnt-1))==0)
                 res++;
         }
         
         preOrder(root->left);
         preOrder(root->right);
         
-        cnt[root->val]--;
+        cnt^= (1<<root->val);
+        
+        return;
     }
     
     int pseudoPalindromicPaths (TreeNode* root) {
-        cnt.clear();
-        cnt.resize(10,0);
-        
         preOrder(root);
         
         return res;

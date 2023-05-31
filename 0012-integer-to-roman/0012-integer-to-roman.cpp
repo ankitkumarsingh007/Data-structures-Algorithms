3c78fdf2d5dd0b22b683fcb235f3f7b42bac573b
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2023-05-31 23:13:17 +0530

    Time: 13 ms (35.70%), Space: 8.8 MB (36.18%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,66 +1,18 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string s="";
+        string res="";
         
-        int a=num/1000;
-        num%=1000;
-        while(a--)
-            s+='M';
-
-        if(num>=900)
-        {
-            s+="CM";
-            num-=900;
-        }
-        if(num>=500){
-            s+='D';
-            num-=500;
-        }
-        if(num>=400){
-            s+="CD";
-            num-=400;
-        }
-
-        a=num/100;
-        while(a--)
-            s+='C';
-
-        num%=100;
-        if(num>=90){
-            s+="XC";
-            num-=90;
-        }
-        if(num>=50){
-            s+='L';
-            num-=50;
-        }
-        if(num>=40){
-            s+="XL";
-            num-=40;
-        }
-
-        a=num/10;
-        num%=10;
-        while(a--)
-            s+='X';
-
-        if(num==9){
-            num-=9;
-            s+="IX";
-        }
-        if(num>=5){
-            num-=5;
-            s+='V';
-        }
-        if(num==4){
-            num-=4;
-            s+="IV";
-        }
-
-        while(num--)
-            s+='I';
+        vector<int> val={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<string> s={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         
-        return s;
+        for(int i=0;i<13;i++){
+            while(num>=val[i]){
+                res+=s[i];
+                num-=val[i];
+            }
+        }
+        
+        return res;
     }
 };

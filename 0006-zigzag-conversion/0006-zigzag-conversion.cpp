142808f0ea41f6c2a122ded730aacb84f0a9e1ee
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2023-06-01 16:29:58 +0530

    Time: 3 ms (98.43%), Space: 8.3 MB (84.29%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
     string convert(string s, int l) {
-        int n=s.size();
-        string res="";
-        
-        if(l==1)
+            int n=s.size();
+            string res="";
+
+            if(l==1)    //corner case
             return s;
-        
-        for(int i=0;i<l;i++){
-            int j=i;
-            
-            if(i==0 || i==l-1){
-                while(j<n){
-                    res+=s[j];
-                    j+=(2*l-2);
-                }
-            }
-            else{
-                while(j<n){
-                    res+=s[j];
-                    j+=(2*(l-i-1));
-                    if(j<n){
+
+            int gap=2*l-2;
+            for(int i=0;i<l;i++)
+            {
+                int j=i;
+                if(i==0 || i==l-1)  //special care
+                {
+                    while(j<n)
+                    {
                         res+=s[j];
-                        j+=(2*i);
+                        j+=gap;
+                    }
+                }
+                else               //regular care
+                {
+                    int gap2=2*i,gap1=gap-gap2;
+                    while(j<n)
+                    {
+                        res+=s[j];
+                        j+=gap1;
+                        if(j<n)
+                        {
+                            res+=s[j];
+                            j+=gap2;
+                        }
                     }
                 }
             }
+
+            return res;
         }
-        return res;
-    }
 };

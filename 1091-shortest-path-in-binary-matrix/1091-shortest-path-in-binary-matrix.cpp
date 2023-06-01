539c7a4af5f17d938cdd177d71849b41d85df359
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2023-06-01 10:42:05 +0530

    Time: 68 ms (81.43%), Space: 19.2 MB (93.42%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -12,33 +12,32 @@ public:
     
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int x[8]={-1,-1,-1,0,0,1,1,1},y[8]={-1,0,1,-1,1,-1,0,1};
-        
         m=grid.size(),n=grid[0].size();
-        vector<vector<int>> dis(m,vector<int>(n,INT_MAX));
         
         queue<pp> q;
-        if(grid[0][0]==0){
-            q.push({0,0});
-            dis[0][0]=1;
-            
-            if(m==1 && n==1)
-                return 1;
-        }
+        if(grid[0][0]!=0)
+            return -1;
+        
+        grid[0][0]=1;
+        q.push({0,0});
+        if(m==1 && n==1)
+            return 1;
         
         while(!q.empty()){
             pp p=q.front();
             q.pop();
             int a=p.first,b=p.second;
+            int a1,b1;
             
             for(int i=0;i<8;i++){
-                int a1=a+x[i],b1=b+y[i];
+                a1=a+x[i],b1=b+y[i];
                 
-                if(isvalid(a1,b1) && dis[a1][b1]==INT_MAX && grid[a1][b1]==0){
-                    dis[a1][b1]=dis[a][b]+1;
+                if(isvalid(a1,b1) && grid[a1][b1]==0){
+                    grid[a1][b1]=grid[a][b]+1;
                     q.push({a1,b1});
                     
                     if(a1==(m-1) && b1==(n-1))
-                        return dis[a1][b1];
+                        return grid[a1][b1];
                 }
             }
         }
@@ -46,3 +45,4 @@ public:
         return -1;
     }
 };
+

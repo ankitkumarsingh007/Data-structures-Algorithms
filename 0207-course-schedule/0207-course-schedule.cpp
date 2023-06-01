502971c8293b1ca2acd2378d99ee8e869fa0785c
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2023-06-01 11:08:06 +0530

    Time: 26 ms (45.84%), Space: 13.6 MB (68.12%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        vector<int> in(numCourses,0);
+        
+        for(auto v:prerequisites){
+            adj[v[0]].push_back(v[1]);
+            in[v[1]]++;
+        }
+        
+        int cnt=0;
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(in[i]==0)
+                q.push(i);
+        }
+        
+        while(!q.empty()){
+            int x=q.front();
+            q.pop();
+            cnt++;
+            
+            for(int a:adj[x]){
+                in[a]--;
+                if(in[a]==0)
+                    q.push(a);
+            }
+        }
+        
+        return cnt==numCourses;
+    }
+};

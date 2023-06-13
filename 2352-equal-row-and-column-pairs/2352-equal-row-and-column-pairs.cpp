591f56245ded5153ddec9aaf8e13ea1d689e2d22
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2023-06-13 14:26:09 +0530

    Time: 259 ms (37.80%), Space: 46.3 MB (13.11%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        int n=grid.size(),res=0;
+        unordered_map<string,int> mp;
+        
+        for(auto v:grid){
+            string s="";
+            for(int a:v){
+                if(s!="")
+                    s+='&';
+                s+=to_string(a);
+            }
+            mp[s]++;
+        }
+        
+        for(int i=0;i<n;i++){
+            string s="";
+            for(int j=0;j<n;j++){
+                if(s!="")
+                    s+='&';
+                s+=to_string(grid[j][i]);
+            }
+            
+            res+=mp[s];
+        }
+        
+        return res;
+    }
+};

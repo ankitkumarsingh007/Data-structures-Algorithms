cbe331e19bcdf5f2ed053a0147ac838701ab6fce
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2025-01-22 08:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (91.03%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,21 +2,36 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size(),n=matrix[0].size();
-        vector<bool> row(m,false),column(n,false);
+
+        bool firstColumnHasZero = false;
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!matrix[i][j]){
-                    row[i]=true;
-                    column[j]=true;
+                if(matrix[i][j]==0){
+                    if(i==0){
+                        firstColumnHasZero=true;
+                    }else{
+                        matrix[i][0]=0;
+                        matrix[0][j]=0;
+                    }
                 }
             }
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(row[i] || column[j])
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(!matrix[i][0] || !matrix[0][j])
                 matrix[i][j]=0;
+            }
+        }
+
+        if(!matrix[0][0]){
+            for(int i=1;i<m;i++)
+            matrix[i][0]=0;
+        }
+        if(firstColumnHasZero){
+            for(int i=0;i<n;i++){
+                matrix[0][i]=0;
             }
         }
 

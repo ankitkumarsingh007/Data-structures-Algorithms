ae0215959704eba4160d066ae912ba92d2021a77
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2025-08-13 18:24:08 +0530

    Time: 12 ms (5.68%) | Memory: 8.8 MB (97.26%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
-    typedef long long ll;
     bool isPowerOfThree(int n) {
-        if(n==1){
-            return true;
-        }
-        if(n<3)
-        return false;
-
-        ll r = ceil(log(n)/log(3)) , l = 1;
-
-        while(l<=r){
-            ll mid = l+(r-l)/2;
-            ll val = pow(3,mid);
-
-            if(val>n){
-                r=mid-1;
-            }else if(val<n){
-                l=mid+1;
-            }else{
-                return true;
-            }
+        while(n>=3){
+            if(n%3!=0)
+            return false;
+            n/=3;
         }
 
-        return false;
+        return n==1;
     }
 };

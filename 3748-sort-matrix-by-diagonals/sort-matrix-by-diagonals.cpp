b4e19c0d05e61c8ad761497a9bf58704c02b232f
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2025-08-28 21:33:07 +0530

    Time: 10 ms (55.76%) | Memory: 44.2 MB (58.02%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
+        vector<priority_queue<int>> lowerHalf(n);
+        vector<priority_queue<int, vector<int>, greater<int>>> upperHalf(n);
 
-        vector<int> temp;
-        for (int k = 1; k < n; k++) {
-            int i = 0, j = k, x = 0;
-            temp.clear();
-
-            while (j < n) {
-                temp.push_back(grid[i++][j++]);
-            }
-            sort(temp.begin(), temp.end());
-            i = 0, j = k;
-            while (j < n) {
-                grid[i++][j++] = temp[x++];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = i - j;
+                if (val >= 0) {
+                    lowerHalf[val].push(grid[i][j]);
+                } else {
+                    upperHalf[abs(val)].push(grid[i][j]);
+                }
             }
         }
 
-        for (int k = 0; k < n; k++) {
-            int i = k, j = 0, x = 0;
-            temp.clear();
-
-            while (i < n) {
-                temp.push_back(grid[i++][j++]);
-            }
-            sort(temp.begin(), temp.end(), greater<int>());
-
-            i = k, j = 0;
-            while (i < n) {
-                grid[i++][j++] = temp[x++];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = i - j;
+                if (val >= 0) {
+                    grid[i][j] = lowerHalf[val].top();
+                    lowerHalf[val].pop();
+                } else {
+                    grid[i][j] = upperHalf[abs(val)].top();
+                    upperHalf[abs(val)].pop();
+                }
             }
         }
 

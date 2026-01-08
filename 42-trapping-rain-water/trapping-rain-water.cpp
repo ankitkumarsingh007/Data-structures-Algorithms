9a86886c1c06531938156107600a78f829e9357f
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2026-01-09 00:37:04 +0530

    Time: 3 ms (18.20%) | Memory: 27.2 MB (36.83%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -7,14 +7,13 @@ public:
         for (int i = 1; i < n; i++) {
             lmax[i] = max(lmax[i - 1], height[i - 1]);
         }
-
         for (int i = n - 2; i >= 0; i--) {
             rmax[i] = max(rmax[i + 1], height[i + 1]);
         }
 
         int res = 0;
         for (int i = 1; i < n - 1; i++) {
-            res += max(min(lmax[i], rmax[i]) - height[i], 0);
+            res += (max(min(lmax[i], rmax[i]) - height[i], 0));
         }
 
         return res;

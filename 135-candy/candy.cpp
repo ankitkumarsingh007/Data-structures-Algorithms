ecd703b6ddee6b574a320cc695a534b88e357efd
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2026-01-09 00:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (70.55%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> arr(ratings.size(), 1);
+        for (int i = 1; i < ratings.size(); i++) {
+            if (ratings[i] > ratings[i - 1])
+                arr[i] = arr[i - 1] + 1;
+        }
+        for (int i = ratings.size() - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1])
+                arr[i] = max(arr[i], arr[i + 1] + 1);
+        }
+
+        int res = 0;
+        for (int a : arr) {
+            res += a;
+        }
+
+        return res;
+    }
+};

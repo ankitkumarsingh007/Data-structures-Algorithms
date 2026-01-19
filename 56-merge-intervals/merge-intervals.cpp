454aeecc0da4de42d49ef201de75a725d982dce0
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2026-01-19 23:15:59 +0530

    Time: 3 ms (89.45%) | Memory: 23.9 MB (32.63%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,6 +1,10 @@
-bool comp(vector<int> a, vector<int> b) {
-    return a[0] != b[0] ? a[0] < b[0] : a[1] < b[1];
+bool comp(vector<int>& a, vector<int>& b) {
+    if (a[0] == b[0]) {
+        return a[1] < b[1];
+    }
+    return a[0] < b[0];
 }
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
@@ -11,15 +15,13 @@ public:
             n = intervals.size();
 
         for (int i = 1; i < n; i++) {
-            if (intervals[i][0] > end) {
-                res.push_back({start, end});
-                start = intervals[i][0];
-                end = intervals[i][1];
-            } else {
+            if (end >= intervals[i][0]) {
                 end = max(end, intervals[i][1]);
+            } else {
+                res.push_back({start, end});
+                start = intervals[i][0], end = intervals[i][1];
             }
         }
-
         res.push_back({start, end});
 
         return res;

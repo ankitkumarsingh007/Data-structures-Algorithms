4ed345e59e1149b9c39376bd9b44e3fe3803751f
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2026-03-21 09:40:05 +0530

    Time: 375 ms (10.30%) | Memory: 21.5 MB (96.43%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> res;
+        int len = words[0].size(), n = s.size();
+        int totalLen = len * words.size();
+
+        int l = 0, r = 0;
+        unordered_map<string, int> freq;
+        for (string s : words) {
+            freq[s]++;
+        }
+
+        for (int pos = 0; pos < min(len, n - totalLen + 1); pos++) {
+            l = pos, r = pos;
+            unordered_map<string, int> curr;
+            while (r <= (n - len) && l <= (n - totalLen)) {
+                string s1 = s.substr(r, len);
+                if (freq.count(s1) != 0) {
+                    curr[s1]++;
+                    if (curr[s1] > freq[s1]) {
+                        while (curr[s1] != freq[s1]) {
+                            string remove = s.substr(l, len);
+                            curr[remove]--;
+                            l += len;
+                        }
+                    }
+                    r += len;
+                    if ((r - l) == totalLen)
+                        res.push_back(l);
+                } else {
+                    curr.clear();
+                    l = r + len;
+                    r = l;
+                }
+                cout << l << " " << r << " " << s1 << endl;
+            }
+        }
+
+        return res;
+    }
+};

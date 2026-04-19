d7bd915617694d1282125c09d6fa5ca974865de0
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2026-04-19 20:41:49 +0530

    Time: 163 ms (40.53%) | Memory: 88.4 MB (77.59%) - LeetSync

---
+++ 4257-sum-of-sortable-integers/sum-of-sortable-integers.cpp
@@ -0,0 +1,54 @@
+typedef pair<int, int> pp;
+class Solution {
+public:
+    pp isSortedRotated(vector<int>& nums, int l, int r) {
+        int mxPos =
+            max_element(nums.begin() + l, nums.begin() + r + 1) - nums.begin();
+        int mnPos =
+            min_element(nums.begin() + l, nums.begin() + r + 1) - nums.begin();
+        
+        for(int i=l;i<r;i++){
+            if(nums[i]==nums[mxPos] && nums[i+1]==nums[mnPos]){
+                mnPos = i+1, mxPos = i;
+                break;
+            }
+        }
+
+        if (is_sorted(nums.begin() + l, nums.begin() + r + 1)) {
+            return {nums[mnPos], nums[mxPos]};
+        }
+        if ((mnPos - mxPos) == 1 && nums[l] >= nums[r]) {
+            if (is_sorted(nums.begin() + l, nums.begin() + mnPos) &&
+                is_sorted(nums.begin() + mnPos, nums.begin() + r + 1))
+                return {nums[mnPos], nums[mxPos]};
+        }
+        return {-1, -1};
+    }
+    int sortableIntegers(vector<int>& nums) {
+        vector<int> arr;
+        int n = nums.size();
+
+        for (int i = 1; i <= n; i++) {
+            if ((n % i) == 0)
+                arr.push_back(i);
+        }
+
+        int res = 0;
+        for (int k : arr) {
+            int l = 0;
+            pp prev = {};
+            for (l = 0; l < n; l += k) {
+                pp curr = isSortedRotated(nums, l, l + k - 1);
+                if (curr.first == -1)
+                    break;
+                if (prev.first && prev.second > curr.first)
+                    break;
+                prev  = curr;
+            }
+
+            if (l >= n)
+                res+=k;
+        }
+        return res;
+    }
+};

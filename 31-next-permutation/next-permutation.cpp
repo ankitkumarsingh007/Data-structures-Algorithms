f9a13451aa8d170b3f701588ac86ef9211caca4e
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2026-08-27 20:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (82.74%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& arr) {
-        int n=arr.size();
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
 
-        int i=n-1;
+        int r = n - 1;
+        while (r != 0 && nums[r - 1] >= nums[r])
+            r--;
 
-        for(i;i>0;i--){
-            if(arr[i]>arr[i-1]){
-                int j=i+1;
-                for(j;j<n;j++){
-                    if(arr[j]<=arr[i-1])
-                    break;
-                }
-
-                swap(arr[i-1],arr[j-1]);
-                sort(arr.begin()+i,arr.end());
-                break;
-            }
+        if (r == 0) {
+            reverse(nums.begin(), nums.end());
+            return;
         }
 
-        if(i==0)
-        sort(arr.begin(),arr.end());
+        int l = r - 1, pos = r;
+        for (int i = r; i < n; i++) {
+            if (nums[i] > nums[l])
+                pos = i;
+        }
+
+        swap(nums[l], nums[pos]);
+
+        // Revesing decreasing sequence makes seuqence sorted
+        reverse(nums.begin() + r, nums.end());
+        return;
     }
 };

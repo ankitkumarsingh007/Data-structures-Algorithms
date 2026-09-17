aa7f01dfa70a465a0a9dafdafbba08b3efa85305
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2026-09-17 23:08:38 +0530

    Time: 75 ms (74.86%) | Memory: 138.5 MB (79.57%) - LeetSync

--- 4344-minimum-operations-to-make-every-element-palindromic/minimum-operations-to-make-every-element-palindromic.cpp
+++ 4344-minimum-operations-to-make-every-element-palindromic/minimum-operations-to-make-every-element-palindromic.cpp
@@ -1,33 +1,47 @@
-vector<long long> P[2];
+typedef long long ll;
 
-int init = []() {
-    long long M = 1e9;
-    for (int v = 1; v < 100000; ++v) {
-        string s = to_string(v);
+vector<ll> p[2];
+int precompute = []() {
+    ll lim = 1e9;
+    for (ll i = 1; i < 1e5; i++) {
+        string s = to_string(i);
         string r = s;
         reverse(r.begin(), r.end());
-        long long a1 = stoll(s.substr(0, s.size() - 1) + r);
-        long long a2 = stoll(s + r);
-        if (a1 < M) P[a1 & 1].push_back(a1);
-        if (a2 < M) P[a2 & 1].push_back(a2);
+
+        // String of odd length
+        ll v1 = stoll(s.substr(0, s.size() - 1) + r);
+        // String of even length
+        ll v2 = stoll(s + r);
+
+        if (v1 <= lim)
+            p[v1 & 1].push_back(v1);
+        if (v2 <= lim)
+            p[v2 & 1].push_back(v2);
     }
-    sort(P[0].begin(), P[0].end());
-    sort(P[1].begin(), P[1].end());
+    sort(p[0].begin(), p[0].end());
+    sort(p[1].begin(), p[1].end());
     return 0;
 }();
 
 class Solution {
 public:
-    long long minOperations(vector<int>& A) {
-        long long res = 0;
-        for (int a : A) {
-            auto& p = P[a & 1];
-            int i = lower_bound(p.begin(), p.end(), a) - p.begin();
-            if (i >= p.size()) i = p.size() - 1;
-            long long d1 = abs(a - p[i]);
-            long long d2 = i > 0 ? abs(a - p[i - 1]) : d1;
-            res += min(d1, d2) / 2;
+    long long minOperations(vector<int>& nums) {
+        ll res = 0;
+
+        for (int a : nums) {
+            auto& p1 = p[a & 1];
+
+            int i = lower_bound(p1.begin(), p1.end(), a) - p1.begin();
+
+            if (i >= p1.size())
+                i = p1.size() - 1;
+
+            ll d1 = abs(a - p1[i]) / 2;
+            ll d2 = i > 0 ? abs(a - p1[i - 1]) / 2 : d1;
+
+            res += min(d1, d2);
         }
+
         return res;
     }
 };
